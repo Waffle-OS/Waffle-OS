4d0f6d2d1f4efe87ed6877e11e7339f20bfa6a89
thewindboi <labaslabaslabaslabaslqbas@gmail.com>
2023-11-23 19:51:53 +0000

    decided to make the os 32bit been slacking off the past few days lmao also decided to change the comments up a lil bit

--- src/bootloader/boot.asm
+++ src/bootloader/boot.asm
@@ -1,113 +1,117 @@
 [ORG 0x7c00]
+[BITS 16]
 %define NEWL 13, 10
+%define VID_MEM 0xb8000
+%define CODE_SEG gdt_data.code-gdt_data
+%define DATA_SEG gdt_data.data-gdt_data
 
-;;;;;;;;;;  ;;;;;;;;;;
-;
-;;   FIRST SECTOR   ;;
-;
-;;;;;;;;;;  ;;;;;;;;;;
+;=============================
+;                            |
+; BOOT SECTOR                |
+;                            |
+;=============================
 
-start:  jmp     main
+start:  jmp     enter32
+
+;=============================
+;                            |
+; FAT data                   |
+;                            |
+;=============================
 
 FAT12:
-        db      "MSWIN4.1"      ; OEM identifier
-        dw      0x0200          ; Bytes per sector (all numbers are in their big endian forms, because nasm converts them to little endian)
-        db      0x01            ; Sectors per cluster
-        dw      0x002           ; Reserved sectors (for rest of boot loader)
-        db      0x02            ; Number of FATs
-        dw      0x00e0          ; Root directory entries (not sure what they are)
-        dw      0x0b40          ; Total sectors in the logical volume
-        db      0xf0            ; Media descriptor type
-        dw      0x0009          ; Sectors per fat
-        dw      0x0012          ; Sectors per track
-        dw      0x0002          ; Heads on the disk
-        dw      0x0000, 0x0000  ; Number of hidden sectors
-        dw      0x0000, 0x0000  ; Number of large sectors
-        db      0x00            ; Drive number (value in DL)
-        db      0x00            ; NT flag
-        db      0x29            ; Signature
-        dw      0xe4d7, 0x1585  ; Volume serial ID (I just copied what mkfs.fat put)
-        db      "RANDOM OS  "   ; Volume label string
-        db      "FAT12   "      ; System identifier (useless)
+        db      "MSWIN4.1"              ; OEM identifier
+        dw      0x0200                  ; Bytes per sector (all numbers are in their big endian forms, because nasm converts them to little endian)
+        db      0x01                    ; Sectors per cluster
+        dw      0x0005                  ; Reserved sectors (for kernel)
+        db      0x02                    ; Number of FATs
+        dw      0x00e0                  ; Root directory entries (not sure what they are)
+        dw      0x0b40                  ; Total sectors in the logical volume
+        db      0xf0                    ; Media descriptor type
+        dw      0x0009                  ; Sectors per fat
+        dw      0x0012                  ; Sectors per track
+        dw      0x0002                  ; Heads on the disk
+        dw      0x0000, 0x0000          ; Number of hidden sectors
+        dw      0x0000, 0x0000          ; Number of large sectors
+        db      0x00                    ; Drive number (value in DL)
+        db      0x00                    ; NT flag
+        db      0x29                    ; Signature
+        dw      0xe4d7, 0x1585          ; Volume serial ID (I just copied what mkfs.fat put)
+        db      "  RANDOM OS"           ; Volume label string
+        db      "FAT12   "              ; System identifier (useless)
 
+;=============================
+;                            |
+; Enters 32 bit mode         |
+;                            |
+;=============================
 
+enter32:
+        ; Setting up the GDT
 
-data:   
-    BOOT_DISK:  db      0
-    DONE:       db      "done!", NEWL, 0
-    FAILED:     db      "failed.", NEWL, 0
-    ERROR_MES:  db      "Fatal error occured. Please restart computer.", 0
-    START_MES:  db      "Bootloader started... ", NEWL, 0
-    STACK_MES:  db      "Stack initialised...", NEWL, 0
-    LOAD_MESG:  db      "Loading extended bootloader... ", 0
+        cli
+        lgdt    [gdt_data.desc]
+
+        mov     eax, cr0
+        or      eax, 1
+        mov     cr0, eax
+
+        jmp     CODE_SEG:main           ; Clears instruction pipeline
+
+.halt:
+        hlt
+
+;=============================
+;                            |
+; GDT data                   |
+;                            |
+;=============================
+
+gdt_data:
+        .null:
+                dq 0
+        .code:
+                dw 0xffff
+                dw 0
+                db 0
+                db 10011010b
+                db 11001111b
+                db 0
+        .data:
+                dw 0xffff
+                dw 0
+                db 0
+                db 10010010b
+                db 11001111b
+                db 0
+.end:
+
+.desc:
+        dw .end - gdt_data - 1
+        dd gdt_data
+
+[BITS 32]
+
+;=============================
+;                            |
+; The majority of the code   |
+;                            |
+;=============================
 
 main:
-        ; setting up the stack
+        mov     al, 'H'
+        mov     ah, 0x0f
 
-        xor     ax, ax                          
-        mov     es, ax
-        mov     ds, ax
-        mov     bp, 0x8000
-        mov     sp, bp
+        mov     [VID_MEM], ax
 
-        ; Showing boot messages
+        hlt
 
-        mov     ah, 0
-        mov     al, 3
-        int     0x10
-        call getch
-        mov     al, ah
-        call putch
-        mov     si, START_MES
-        call puts
-        mov     si, STACK_MES
-        call puts
-        mov     si, LOAD_MESG
-        call puts
-        mov     [BOOT_DISK], dl
 
-        ; reading the disk
+;=============================
+;                            |
+; Boot signature             |
+;                            |
+;=============================
 
-        mov     ah, 2                   ; code to read from disk
-        mov     al, 1                   ; number of sectors to read
-        mov     ch, 0                   ; cylinder
-        mov     dh, 0                   ; head
-        mov     cl, 2                   ; sector
-        mov     dl, [BOOT_DISK]         ; drive
-        mov     bx, 0x7e00              ; where to place the read data in memory (es:bx)
-        int     0x13
-        jc      .error                  ; in case of failure
-
-        mov     si, DONE
-        call puts
-        call getch
-        mov     dx, 0x604
-        mov     ax, 0x2000
-        out     dx, ax                  ; Shutdowns the PC (only works when emulating on qemu)
-
-        
-        
-.halt:
-        jmp     $
-.error:
-        mov     si, FAILED
-        call puts
-        mov     si, ERROR_MES
-        call puts
-        jmp     .halt
-
-%include "src/bootloader/ttystr.asm"
-
-;
-;; boot signature
-;
 times 510-($-$$) db 0              
 dw 0xaa55
-
-;;;;;;;;;;  ;;;;;;;;;;
-;
-;;  OTHER SECTORS   ;;
-;
-;;;;;;;;;;  ;;;;;;;;;;
-
-times 512 db 'h'
