bdbe7f114ce89b2b642bba2b2bf97c150299f9d1
thewindboi <labaslabaslabaslabaslqbas@gmail.com>
2024-01-26 16:38:00 +0000

    done some stuff also made it print a waffle to the terminal

--- kernel/main.c
+++ kernel/main.c
@@ -1,10 +1,26 @@
 #include "serial/terminal.h"
+#include "interrupts/pic.h"
+#include "kutils/panic.h"
 
 extern void main(void)
 {
-    init_com1();
-    terminal_putch('H');
-    terminal_puts("ello, World!\n");
+    // Initialise COM1 for terminal output.
+    if(init_com1())
+        return;
+    
+    // Remap PIC so that it doesnt overlap with exceptions.
+    pic_remap(0x20, 0x28);
+
+    terminal_puts("\033[37;1m");
+    terminal_puts("_______________\n");
+    terminal_puts("|  _   _   _  |  WELCOME! \n");
+    terminal_puts("| |_| |_| |_| |  WaffleOS 0.0.0-prealpha \n");
+    terminal_puts("|  _   _   _  |\n");
+    terminal_puts("| |_| |_| |_| |\n");
+    terminal_puts("|  _   _   _  |\n");
+    terminal_puts("| |_| |_| |_| |\n");
+    terminal_puts("|_____________|\n");
+
 }
 
 
